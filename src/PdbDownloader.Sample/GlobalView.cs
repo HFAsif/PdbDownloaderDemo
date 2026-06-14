9e3b9cdc6bc0aec8fd764bee54cac076413a6e8e
Asif <HFAsif@live.com>
2026-06-14 12:41:38 +0600

    Comment out global using for System.ReflectionEx

--- src/PdbDownloader.Sample/GlobalView.cs
+++ src/PdbDownloader.Sample/GlobalView.cs
@@ -4,6 +4,6 @@ global using DownloadItem = HelperClass.All.ImportantElements_DownloadItem;
 global using Kernal32Help = HelperClass.All.ImportantElements_Kernal32Help;
 global using PdbDownloadHelper = HelperClass.All.ImportantElements_PdbDownloadHelper;
 #if NET35 || NET20
-global using System.ReflectionEx;
+//global using System.ReflectionEx;
 global using FrameWorkEngine;
 #endif
